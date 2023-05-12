8cfe4c077b241d11e0dc4e997015bfa6fe997044
Jacob <jdheymans05@gmail.com>
2023-05-12 10:34:53 +1000

    added puase menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject character;
+    public GameObject pauseCam;
+    public GameObject tpUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+                character.SetActive(true);
+                pauseCam.SetActive(false);
+                tpUI.SetActive(true);
+            }
+            else
+            {
+                Pause();
+                character.SetActive(false);
+                pauseCam.SetActive(true); 
+                tpUI.SetActive(false);
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        
+    }
+}
